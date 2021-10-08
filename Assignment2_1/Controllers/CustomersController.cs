1a80ee56f66c811b6ed1108e990f49c66280b87f
vinashree godse <godse.vinu@outlook.com>
2021-10-08 11:19:09 +0530

    Add project files.

---
+++ Assignment2_1/Controllers/CustomersController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Assignment2_1.Models;
+namespace Assignment2_1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        static List<Customer> Cust = new List<Customer>
+        {
+            new Customer(){CustomerId=1,Name="Vinu",Phone_No=8999,Email="g.vinu@h",City="Pune" }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(Cust);
+        }
+        public IActionResult Details(int id)
+        {
+            var Check_Customer = Cust.Find(obj => obj.CustomerId == id);
+            if(Check_Customer== null)
+            {
+                return View("Not Found");
+            }
+            return View(Check_Customer);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer Cust_1)
+        {
+            
+            if (!ModelState.IsValid)
+         {
+                ViewBag.Error = "Please Correct Validation";
+                return View(Cust_1);
+            }
+         Cust.Add(Cust_1);
+        return RedirectToAction("Index");
+        }
+
+       
+        public IActionResult Create()
+        {
+          return View();
+        }
+    }
+}
---
+++ Assignment2_1/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Assignment2_1.Models
+{
+    public class Customer
+    {
+        //[Required(ErrorMessage = "Cannot be Blank")]
+        public int CustomerId { get; set; }
+        
+        [Required(ErrorMessage ="Enter Name")]
+        [StringLength(15, ErrorMessage ="Name should be less than 15")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Enter Email")]
+        public string Email { get; set; }
+
+        [Required (ErrorMessage ="Enter City")]
+        public string City { get; set; }
+
+        //[Required (ErrorMessage ="Enter Phone no")]
+        //[MaxLength(10,ErrorMessage ="Enter 10 digit")]
+        public int Phone_No { get; set; }
+    }
+}
